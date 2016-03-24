eff9f1596fd78ab693ed4a4b9f2fa92c83828ae9
zizy <mezizy@live.com>
2016-03-24 17:03:04 +0800

    no msg

---
+++ front/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Zizy on 3/24/16.
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+<h1>Hello, world!</h1>,
+    document.getElementById('example')
+);
