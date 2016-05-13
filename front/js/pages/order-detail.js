53d4b84abcb5e6845d70a1a395fa460ba15d503b
tion846 <aasd609@yahoo.com.tw>
2016-05-13 21:45:59 +0800

    passValue

---
+++ front/js/pages/order-detail.js
@@ -0,0 +1,40 @@
+'use strict';
+/**
+ * Created by Zizy on 4/6/16.
+ */
+let ajaxMethod = require('../ajaxMethods.js');
+let $$ = Dom7;
+let myApp = null, mainView = null;
+let tool = require('../tool.js');
+
+
+class OrderDetailPage { //TODO first
+    constructor(_myApp, _mainView) {
+        myApp = _myApp;
+        mainView = _mainView;
+
+    }
+
+    bind() {
+        myApp.onPageBeforeInit('order-detail', function (page) {//TODO second
+            this.grpId = page.query.grpId || this.grpId;
+
+            console.log(this.grpId);
+
+            tool.loadTemplateFromJsonPromise(myApp,ajaxMethod.getGroupById(this.grpId), page, ()=> {
+
+
+            });
+
+        });
+
+        myApp.onPageInit('order-detail', (page) => {
+
+            console.log('order-detail Init');
+        });
+    }
+
+}
+
+module.exports = OrderDetailPage;////TODO third
+//TODO fourth : 加入到Main.js里面注册bind方法
