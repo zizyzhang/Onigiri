561ed87eab74c5ad47f8d048616c7b3285b9f3d6
tion846 <aasd609@yahoo.com.tw>
2016-07-05 11:46:06 +0800

    orders2

--- front/js/pages/product-detail.js
+++ front/js/pages/product-detail.js
@@ -20,41 +20,34 @@ class ProductDetailPage { //TODO first
     bind() {
         myApp.onPageBeforeInit('proudct-detail', function (page) {//TODO second
             let hostId = cookies.getJSON('user').usrId;
-            console.log('proudct-detail page init');
+            // console.log('proudct-detail page init');
 
             tool.loadTemplateFromJsonPromise(myApp, ajaxMethod.getGroupedOrdersAndSumsByHostIdPromise(hostId), page, function (result) {
-                // console.log(JSON.stringify(result));
 
                 for (let group of result.groupedOrders) {
                     for (let order of group.orders) {
-                        // console.log("ordId" + order.ordId);
-                        // console.log("ordStatus" + );
                         let isChecked = order.ordStatus == 2;
 
                         if (isChecked) {
                             $$('#ordId-' + order.ordId).addClass('completed');
                             $$('#ordId-' + order.ordId).attr('style', 'text-decoration:line-through; color:DarkGray;');
-                            // $$('#ordId-' + order.ordId).attr('style', 'color:blue');
                             $$('#chbox-' + order.ordId).attr('checked', 'checked');
                         }
                     }
                 }
-
-                // text-decoration:line-through;
+                
                 $$(".item-content").click(function () {
                     let ordId = $$(this).dataset().ordId;
                     if ($$('#ordId-' + ordId).hasClass('completed')) {
-                        console.log("hasClass completed");
+                        // console.log("hasClass completed");
                         $$('#ordId-' + ordId).removeClass('completed');
-                        $$('#ordId-' + ordId).removeAttr('style');
-                        // $$('#chbox-' + ordId).removeAttr('checked');
+                        $$('#ordId-' + ordId).removeAttr("style");
                         ajaxMethod.updateOrdStatusPromise(ordId);
                         // mainView.router.refreshPage();
                     } else {
                         $$('#ordId-' + ordId).addClass('completed');
                         $$('#ordId-' + ordId).attr('style', 'text-decoration:line-through; color:DarkGray;');
                         ajaxMethod.updateOrdStatusPromise(ordId);
-                        // $$('#chbox-'+ordId).attr('checked','checked');
                     }
                 });
 
