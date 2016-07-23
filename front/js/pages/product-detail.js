4981844a2d03f3321a38e39f29539ba3b8fbabc0
tion846 <aasd609@yahoo.com.tw>
2016-07-23 18:16:09 +0800

    useResult

--- front/js/pages/product-detail.js
+++ front/js/pages/product-detail.js
@@ -8,7 +8,7 @@ let myApp = null, mainView = null;
 let tool = require('../tool.js');
 let cookies = require('js-cookie');
 let Vue = require('vue');
-
+let _ = require('lodash');
 
 class ProductDetailPage { //TODO first
     constructor(_myApp, _mainView) {
@@ -23,9 +23,59 @@ class ProductDetailPage { //TODO first
             console.log('proudct-detail page init');
 
 
-            tool.loadTemplateFromJsonPromise(myApp, ajaxMethod.getGroupedOrdersAndSumsByHostIdPromise(hostId), page, function (result) {
-                // console.log(JSON.stringify(result));
+            ajaxMethod.getGroupedOrdersAndSumsByHostIdPromise(hostId).then(function (result) {
 
+                // let groupOrder = new Vue({
+                //     el: '#grpOrd',
+                //     data: result
+                // });
+                
+                
+                let GrpUsersOrders = {
+                    GrpUsersOrders:[]
+                };
+
+                for (let grpOrd of result.groupedOrders) {
+                    let neGUO = {};
+                    let uos = [];
+                    let grpComments = grpOrd.group.grpComments;
+
+                    for (let order of grpOrd.orders) {
+                        order.dish.ordNum = order.ordNum;
+                        let uosobj=uos.find(u=>u.usrId === order.usrId);
+
+                        if ( !uosobj ) {
+                            uos.push({
+                                usrId: order.usrId,
+                                usrName: order.usrName,
+                                usrAmount: 0,
+                                usrDishes: [order.dish],
+                                usrComments: _.filter(grpComments,(com) => com.usrId === order.usrId),
+                                usrOrdIds:[{ordId:order.ordId}]
+                            });
+                            // console.log("====uos" + JSON.stringify(uos));
+                        }else{
+                            uosobj.usrDishes.push(order.dish);
+                            uosobj.usrOrdIds.push({ordId:order.ordId});
+                        }
+                    }
+                    // console.log("====uos" + JSON.stringify(uos));
+
+                    neGUO.group = grpOrd.group;
+                    neGUO.usrOrders = uos;
+                    GrpUsersOrders.GrpUsersOrders.push(neGUO);
+                }
+                // console.log("====result.groupedOrders" + JSON.stringify(result.groupedOrders));
+                console.log("====GrpUsersOrders" + JSON.stringify(GrpUsersOrders));
+
+
+                let groupOrder = new Vue({
+                    el: '#grpOrd',
+                    data: GrpUsersOrders
+                });
+
+
+                
                 for (let groupedOrders of result.groupedOrders) {
                     for (let order of groupedOrders.orders) {
                         let isChecked = order.ordStatus == 2;
@@ -47,8 +97,43 @@ class ProductDetailPage { //TODO first
                     ajaxMethod.updateOrdStatusPromise(ordId, ordStatus);
                 });
 
+                $$(".unPaid").click(function () {
+                    let ordId = $$(this).dataset().ordId;
+                    //ordStatus為訂單狀態(-1:拒絕,0:待審查,1:已確認=未付款,2:已付款)
+                    let ordStatus = 1;
+                    $$('#ordId-' + ordId).removeClass('completed');
+                    $$('#ordId-' + ordId).removeAttr('style');
+                    ajaxMethod.updateOrdStatusPromise(ordId, ordStatus);
+                });
             });
-            
+
+
+            // tool.loadTemplateFromJsonPromise(myApp, ajaxMethod.getGroupedOrdersAndSumsByHostIdPromise(hostId), page, function (result) {
+            //     // console.log(JSON.stringify(result));
+            //
+            //     for (let groupedOrders of result.groupedOrders) {
+            //         for (let order of groupedOrders.orders) {
+            //             let isChecked = order.ordStatus == 2;
+            //
+            //             if (isChecked) {
+            //                 $$('#ordId-' + order.ordId).addClass('completed');
+            //                 $$('#ordId-' + order.ordId).attr('style', 'text-decoration:line-through; color:DarkGray;');
+            //                 $$('#chbox-' + order.ordId).attr('checked', 'checked');
+            //             }
+            //         }
+            //     }
+            //
+            //     $$(".paid").click(function () {
+            //         let ordId = $$(this).dataset().ordId;
+            //         //ordStatus為訂單狀態(-1:拒絕,0:待審查,1:已確認=未付款,2:已付款)
+            //         let ordStatus = 2;
+            //         $$('#ordId-' + ordId).addClass('completed');
+            //         $$('#ordId-' + ordId).attr('style', 'text-decoration:line-through; color:DarkGray;');
+            //         ajaxMethod.updateOrdStatusPromise(ordId, ordStatus);
+            //     });
+            //
+            // });
+
 
         });
     }
